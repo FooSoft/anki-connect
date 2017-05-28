1272d4e6b03b7affb7e01bf59ca1265a18e76a60
Vladislav Chagaev <vchagaev@gmail.com>
2017-05-28 11:41:51 +0300

    fix-api-params-handler

--- AnkiConnect.py
+++ AnkiConnect.py
@@ -445,9 +445,15 @@ class AnkiConnect:
 
     def handler(self, request):
         action = 'api_' + request.get('action', '')
+        params = request.get('params')
+        api_func = getattr(self, action)
+
         if hasattr(self, action):
             try:
-                return getattr(self, action)(**(request.get('params') or {}))
+                if not params:
+                    return api_func()
+
+                return api_func(params)
             except TypeError:
                 return None
 
@@ -460,9 +466,8 @@ class AnkiConnect:
         return self.anki.modelNames()
 
 
-    def api_modelFieldNames(self, modelName):
-        return self.anki.modelFieldNames(modelName)
-
+    def api_modelFieldNames(self, params):
+        return self.anki.modelFieldNames(params.get("modelName"))
 
     def api_addNote(self, note):
         params = AnkiNoteParams(note)
