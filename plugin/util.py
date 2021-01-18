c0cdc8e575b1f686febeab89e627a25d9391006f
Alex Yatskov <alex@foosoft.net>
2021-01-17 22:15:48 -0800

    Update copyrights

--- plugin/util.py
+++ plugin/util.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2020 Alex Yatskov
+# Copyright 2016-2021 Alex Yatskov
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
