008e0c828b2bf23755018363b5dc99a0033b2004
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-05-04 18:05:31 +0200

    Allow 127.0.0.1 and browser extension if localhost allowed

--- plugin/web.py
+++ plugin/web.py
@@ -175,6 +175,12 @@ class WebServer:
             if originStr in webCorsOriginList :
                 corsOrigin = originStr
                 allowed = True
+            elif 'http://localhost' in webCorsOriginList and ( 
+            originStr == 'http://127.0.0.1' or originStr == 'https://127.0.0.1' or # allow 127.0.0.1 if localhost allowed
+            originStr.startswith('http://127.0.0.1:') or originStr.startswith('http://127.0.0.1:') or
+            originStr.startswith('chrome-extension://') or originStr.startswith('moz-extension://') ) : # allow chrome and firefox extension if localhost allowed
+                corsOrigin = originStr
+                allowed = True
         else:
             allowed = True
 
